35d71fd9871834de152aa2173792e60de3770f3c
welentsai <welentsai@gmail.com>
2022-11-27 09:19:31 +0800

    add more test cases

--- src/tests/sum.test.ts
+++ src/tests/sum.test.ts
@@ -12,5 +12,13 @@ describe('#sum', () => {
     it("return 0 with no numbers", () => {
         expect(sum()).toBe(0)
     })
+
+    it("return same number with one number", () => {
+        expect(sum(1)).toBe(1)
+    })
+
+    it("return total number with multiple numbers", () => {
+        expect(sum(1, 2, 3, 4, 5)).toBe(15)
+    })
 })
 
